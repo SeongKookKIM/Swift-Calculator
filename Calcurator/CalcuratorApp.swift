aeebaa8eda628ffe16ae7356bca810be47e4192b
SeongKookKIM <ksk8646@naver.com>
2024-04-18 20:19:09 +0900

    Initial commit

---
+++ Calcurator/CalcuratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcuratorApp.swift
+//  Calcurator
+//
+//  Created by SeongKook on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcuratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
